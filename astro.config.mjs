20b73e1fd928b66251e79d7f00985531760cfb47
Campbell Turner <turner4965@gmail.com>
2024-10-08 10:32:04 -0700

    removed base blogs and further enhanced site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://campbell-turner.com"
+  site: "https://campbell-turner.com",
+  integrations: [preact()]
 });
