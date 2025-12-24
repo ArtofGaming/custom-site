cf3e99f590782130d4e7bcc93660f6b610519e9a
ArtofGaming <turner4965@gmail.com>
2025-12-24 13:20:57 -0700

    added now to navigation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Campbell Turner | Blog',
+    title: 'TheArtofGaming | Blog',
     description: 'Where my musings on creativity, notes on process, and progress updates live',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./blog/*.md')),
